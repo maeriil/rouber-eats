ea5fc3a3cd746b30aa6d5ba3ebbff38dca058969
maeriil <jixanna@gmail.com>
2025-03-28 08:44:59 -0400

    bug fixes and ordering mechanicsm

---
+++ src/client/system/dialogues/run_dialogues.lua
@@ -0,0 +1,186 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local go_back_page = require(ReplicatedStorage.client.app.utility.go_back_page)
+local jecs = require(ReplicatedStorage.pkg.jecs)
+local p = require(ReplicatedStorage.common.ecs.pairs)
+local phases = require(ReplicatedStorage.common.ecs.phases)
+local t = require(ReplicatedStorage.types)
+local uistore = require(ReplicatedStorage.client.app.uistore)
+
+local c = require(ReplicatedStorage.common.ecs.components)
+local pair = jecs.pair
+local for_each_target = require(ReplicatedStorage.common.utility.for_each_target)
+local world = require(ReplicatedStorage.common.ecs.world)
+local __ = jecs.Wildcard
+
+local dialogue_started = world:query(c.MyDialogue):with(c.Start):cached()
+
+local function on_dialogue_started()
+	local it = dialogue_started:iter()
+	local my_id, configs = it()
+	if not my_id then
+		return
+	end
+
+	-- When the dialogue first starts, we want to disable most of humanoid's movemenet
+	-- and reenable it after dialogue is completed
+	local humanoid = world:get(c.Client, c.Humanoid)
+	assert(humanoid ~= nil, `Humanoid must exist on client's character`)
+	humanoid:SetStateEnabled(Enum.HumanoidStateType.Jumping, false)
+	humanoid:SetStateEnabled(Enum.HumanoidStateType.Running, false)
+
+	-- This is the tree, and it should contain things like the author name, and the intial
+	-- text that we populate in the uistore
+	local dialogue_tree = configs.id
+	local author = world:get(dialogue_tree, c.DialogueAuthor) :: string
+
+	configs.system_completed = false
+	configs.action_completed = true
+
+	uistore.DialogueAuthor(author)
+	uistore.MyDialogueId(my_id)
+	world:remove(my_id, c.Start)
+end
+
+local dialogue_cleanup_query = world:query(c.MyDialogue):with(c.Cleanup):cached()
+local function on_dialogue_cleanup()
+	local it = dialogue_cleanup_query:iter()
+	local my_id, configs = it()
+	if not my_id then
+		return
+	end
+
+	-- When cleanup occurs, we want to reset all the changes we made when we first started dialogue
+	-- just refer to that function and revert changes
+
+	local humanoid = world:get(c.Client, c.Humanoid)
+	assert(humanoid ~= nil, `Humanoid must exist on client's character`)
+	humanoid:SetStateEnabled(Enum.HumanoidStateType.Jumping, true)
+	humanoid:SetStateEnabled(Enum.HumanoidStateType.Running, true)
+
+	-- WE want to update the UI so we no longer show the dialogue pages
+	go_back_page()
+
+	-- Finally, delete the entity and delete the parent node. Ensure that we delete all of the
+	-- dialogue tree, Verify this in jabby
+
+	world:delete(configs.id)
+	world:delete(my_id)
+end
+
+local dialogue_general = world:query(c.MyDialogue):without(c.Cleanup):cached()
+local function queue_next_dialogue_options()
+	local it = dialogue_general:iter()
+	local my_id, configs = it()
+	if not my_id or not configs.action_completed then
+		return
+	end
+
+	local node = configs.current_position
+
+	if world:has(node, c.DialogueChoiceContainer) then
+		-- If the current is dialogue choice container and user clicked on some action
+		-- we want to grab it , verifiy it exists, then move our position to that index
+		-- We also want to clear the dialogue choices source so it does the clearing animation
+
+		local choice_id = configs.dialogue_choice :: t.Entity
+		node = choice_id
+		uistore.DialogueChoices({})
+	end
+
+	-- Now, it means completed is true, which means we want to iterate over the next
+	-- on the tree
+	local next_id = world:target(node, c.Next)
+	if next_id == nil then
+		-- If no next is found, this means we have reached the end of the dialogue.
+		-- This means that we should end the dialogue and start to perform clean
+		warn(`TODO: Now we will need to perform dialogue cleanup`)
+		world:add(my_id, c.Cleanup)
+		return
+	end
+
+	configs.current_position = next_id
+
+	-- If we do find next, now we need to determine what kind of dialogue this is
+	-- If it is a condition, we need to start to build the future and spawn a seperate entity for it
+	-- If it is a goto, we need to find traverse up until we find out
+
+	if world:has(next_id, c.DialogueResponse) then
+		--// TODO
+		-- If the next response is a choice, we want to automatically display it
+		-- We might need to introduce another source for this
+
+		world:add(my_id, c.DialogueResponse)
+	elseif world:has(next_id, c.DialogueChoiceContainer) then
+		-- This means that now we are in a choice area, so now we have to grab allthe choices
+		-- and add them to the dialogue choice array
+		-- Also, when the user makes a selection, we need to clear up the array as well
+		world:add(my_id, c.DialogueChoiceContainer)
+	end
+
+	configs.system_completed = false
+end
+
+local my_display_response_query = world:query(c.MyDialogue):with(c.DialogueResponse):cached()
+local function display_response_to_ui()
+	local it = my_display_response_query:iter()
+	local my_id, configs = it()
+	if not my_id or configs.system_completed then
+		return
+	end
+
+	local current_node = configs.current_position
+	local text = world:get(current_node, c.DialogueText) :: string
+
+	uistore.DialogueResponse(text)
+	uistore.DialogueResponseId(current_node)
+
+	-- Now, we just need to wait until the user clicks or does some action
+	world:remove(my_id, c.DialogueResponse)
+	configs.system_completed = true
+	configs.action_completed = false
+
+	warn(`DISPLAYED THE RESPONSE MESSAGE TO UI!!!`)
+end
+
+local my_display_choices_query = world:query(c.MyDialogue):with(c.DialogueChoiceContainer):cached()
+local function display_choices_to_ui()
+	local it = my_display_choices_query:iter()
+	local my_id, configs = it()
+	if not my_id or configs.system_completed then
+		return
+	end
+
+	local current_node = configs.current_position
+	local choices_list = {} :: { { id: t.Entity, text: string } }
+
+	for_each_target(current_node, c.Next, function(id: jecs.Entity<unknown>)
+		-- local choice_node = pair(c.Next, id)
+		local text = world:get(id, c.DialogueText) :: string
+
+		table.insert(choices_list, {
+			id = id,
+			text = text,
+		})
+	end)
+
+	uistore.DialogueChoices(choices_list)
+
+	world:remove(my_id, c.DialogueChoiceContainer)
+	configs.system_completed = true
+	configs.action_completed = false
+end
+
+local function system()
+	on_dialogue_started()
+	queue_next_dialogue_options()
+	display_response_to_ui()
+	display_choices_to_ui()
+	on_dialogue_cleanup()
+end
+
+return {
+	name = script.Name,
+	system = system,
+	phase = phases.Update,
+	runConditions = {},
+}
