e2e54ca001f234ab4013f2c41f926d24c6bb1f76
maeriil <jixanna@gmail.com>
2025-03-30 03:02:33 -0400

    bump version up

--- src/client/system/dialogues/run_dialogues.lua
+++ src/client/system/dialogues/run_dialogues.lua
@@ -9,6 +9,7 @@ local uistore = require(ReplicatedStorage.client.app.uistore)
 local c = require(ReplicatedStorage.common.ecs.components)
 local pair = jecs.pair
 local for_each_target = require(ReplicatedStorage.common.utility.for_each_target)
+local future = require(ReplicatedStorage.pkg.future)
 local world = require(ReplicatedStorage.common.ecs.world)
 local __ = jecs.Wildcard
 
@@ -60,6 +61,11 @@ local function on_dialogue_cleanup()
 	-- WE want to update the UI so we no longer show the dialogue pages
 	go_back_page()
 
+	uistore.DialogueResponse("")
+	uistore.DialogueResponseId(nil :: never)
+	uistore.DialogueAuthor("")
+	uistore.MyDialogueId(nil :: never)
+	uistore.DialogueChoices({})
 	-- Finally, delete the entity and delete the parent node. Ensure that we delete all of the
 	-- dialogue tree, Verify this in jabby
 
@@ -76,20 +82,37 @@ local function queue_next_dialogue_options()
 	end
 
 	local node = configs.current_position
-
 	if world:has(node, c.DialogueChoiceContainer) then
 		-- If the current is dialogue choice container and user clicked on some action
 		-- we want to grab it , verifiy it exists, then move our position to that index
 		-- We also want to clear the dialogue choices source so it does the clearing animation
+		local choice_id = configs.dialogue_choice
 
-		local choice_id = configs.dialogue_choice :: t.Entity
+		if choice_id == nil then
+			--[[
+				if the choice id is nil, then it means player click on something other than choices
+				that invoked action_completed to be true. In this, ignore it and makr action_completed back
+				to false
+			]]
+			configs.action_completed = false
+			return
+		end
+
 		node = choice_id
 		uistore.DialogueChoices({})
+		configs.dialogue_choice = nil
+	elseif world:has(node, c.DialogueIf) then
+		-- We are in condition, and it hasn't been resolved yet, so we just halt all the process\
+		-- until the poll system manually updates thie config
+		return
+	elseif world:has(node, c.DialogueResponse) then
+	else
 	end
 
 	-- Now, it means completed is true, which means we want to iterate over the next
 	-- on the tree
 	local next_id = world:target(node, c.Next)
+
 	if next_id == nil then
 		-- If no next is found, this means we have reached the end of the dialogue.
 		-- This means that we should end the dialogue and start to perform clean
@@ -108,16 +131,64 @@ local function queue_next_dialogue_options()
 		--// TODO
 		-- If the next response is a choice, we want to automatically display it
 		-- We might need to introduce another source for this
-
 		world:add(my_id, c.DialogueResponse)
 	elseif world:has(next_id, c.DialogueChoiceContainer) then
 		-- This means that now we are in a choice area, so now we have to grab allthe choices
 		-- and add them to the dialogue choice array
 		-- Also, when the user makes a selection, we need to clear up the array as well
 		world:add(my_id, c.DialogueChoiceContainer)
+	elseif world:has(next_id, c.DialogueIf) then
+		-- This means we are now at a dialogue If conditoins. Dialogue if is a little tricky, we need
+		-- to basically spawn a new entity with future wrapped on it and then poll it until we
+		-- get back some result. After we do, we automatically move to the next dialogue depending on the
+		-- response / failure
+
+		local callback_to_wrap = world:get(next_id, c.DialogueIf) :: typeof(c.DialogueIf.__T)
+
+		local futured = future.Future.new(callback_to_wrap)
+		local condition_id = world:entity()
+		world:set(condition_id, c.BoolFuture, futured)
+		world:set(condition_id, pair(c.EntityId, c.DialogueIf), next_id)
+		world:set(condition_id, pair(c.EntityId, c.MyDialogue), my_id)
 	end
 
 	configs.system_completed = false
+end
+
+local function poll_dialogue_condition()
+	for id, future, my_id, dialogue_id in
+		world:query(c.BoolFuture, pair(c.EntityId, c.MyDialogue), pair(c.EntityId, c.DialogueIf)):iter()
+	do
+		local poll = future:poll()
+		if not poll:isReady() then
+			continue
+		end
+
+		local unwrap_res = poll:unwrap()
+		if unwrap_res:isErr() then
+			local message = unwrap_res:unwrapErr()
+			task.spawn(error, message)
+
+			world:delete(id)
+		end
+
+		local condition_result = unwrap_res:unwrapOk()
+		local config = world:get(my_id, c.MyDialogue) :: typeof(c.MyDialogue.__T)
+		local success_id = world:target(dialogue_id, c.DialogueSuccess) :: t.Entity
+		local failure_id = world:target(dialogue_id, c.DialogueFailure) :: t.Entity
+
+		if condition_result then
+			-- We should move on to the Dialogue Success
+			config.current_position = success_id
+		else
+			-- We shold move on to the Dialgoue Failure
+			config.current_position = failure_id
+		end
+
+		config.system_completed = true
+		config.action_completed = true
+		world:delete(id)
+	end
 end
 
 local my_display_response_query = world:query(c.MyDialogue):with(c.DialogueResponse):cached()
@@ -138,8 +209,6 @@ local function display_response_to_ui()
 	world:remove(my_id, c.DialogueResponse)
 	configs.system_completed = true
 	configs.action_completed = false
-
-	warn(`DISPLAYED THE RESPONSE MESSAGE TO UI!!!`)
 end
 
 local my_display_choices_query = world:query(c.MyDialogue):with(c.DialogueChoiceContainer):cached()
@@ -153,7 +222,7 @@ local function display_choices_to_ui()
 	local current_node = configs.current_position
 	local choices_list = {} :: { { id: t.Entity, text: string } }
 
-	for_each_target(current_node, c.Next, function(id: jecs.Entity<unknown>)
+	for_each_target(current_node, c.Next, function(id)
 		-- local choice_node = pair(c.Next, id)
 		local text = world:get(id, c.DialogueText) :: string
 
@@ -173,6 +242,7 @@ end
 local function system()
 	on_dialogue_started()
 	queue_next_dialogue_options()
+	poll_dialogue_condition()
 	display_response_to_ui()
 	display_choices_to_ui()
 	on_dialogue_cleanup()
